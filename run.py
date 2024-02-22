e3f6e2cc6c3ccb8b7423b6377933d872cc447c1a
Erik Berger <berger.erik12@gmail.com>
2024-02-22 14:58:48 +0100

    Removing class Board and refactoring code for two players instead

--- run.py
+++ run.py
@@ -1,18 +1,5 @@
 from random import randint
 
-class Board:
-    def __init__(self, size):
-        self.size = size
-        self.grid = [['O' for _ in range(size)] for _ in range(size)]
-
-    def print_board(self):
-        for row in self.grid:
-            print(" ".join(row))
-
-    def place_ship(self, ship_size):
-
-        pass
-
 class Player:
     def take_shot(self):
         while True:
