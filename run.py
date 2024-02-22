cbc3d618011c452ffc3acc34ebef08d248a91af8
Erik Berger <berger.erik12@gmail.com>
2024-02-22 14:54:53 +0100

    Creating Board class and initilizing the board

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class Board:
+    def __init__(self, size):
+        self.size = size
+        self.grid = [['O' for _ in range(size)] for _ in range(size)]
+
+    def print_board(self):
+        for row in self.grid:
+            print(" ".join(row))
+
+    def place_ship(self, ship_size):
+      
+        pass
+
+board = Board(10)
+board.place_ship(3)
+board.print_board()
