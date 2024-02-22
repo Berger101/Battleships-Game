e8dce719dfb0ef37496dbe8b807538c733571aec
Erik Berger <berger.erik12@gmail.com>
2024-02-22 15:29:15 +0100

    Added method for placing ships on the board

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import choice, randint
 
 class Player:
     def __init__(self, name):
@@ -10,6 +10,24 @@ class Player:
         for ship, size in self.ships.items():
             self.place_ship(ship, size)
 
+    def place_ship(self, ship, size):
+        while True:
+            orientation = choice(["horizontal", "vertical"])
+            if orientation == "horizontal":
+                row = randint(0, 9)
+                col = randint(0, 10 - size)
+                if all(self.board[row][col + i] == "." for i in range(size)):
+                    for i in range(size):
+                        self.board[row][col + i] = ship[0]
+                    break
+            else:
+                row = randint(0, 10 - size)
+                col = randint(0, 9)
+                if all(self.board[row + i][col] == "." for i in range(size)):
+                    for i in range(size):
+                        self.board[row + i][col] = ship[0]
+                    break
+
     def display_board(self):
         print(f"\n{self.name}'s Board:")
         print("  0 1 2 3 4 5 6 7 8 9")
@@ -20,6 +38,7 @@ class Player:
         pass
 
 player = Player("Player")
+player.place_ships()
 
 # Display the initial board
 player.display_board()
