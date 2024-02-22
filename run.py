0df8f7f64e9e442fd6c3914b99004c46f9c3ae66
Erik Berger <berger.erik12@gmail.com>
2024-02-22 14:55:55 +0100

    Creating Player class and adding input

--- run.py
+++ run.py
@@ -10,9 +10,29 @@ class Board:
             print(" ".join(row))
 
     def place_ship(self, ship_size):
-      
+
+        pass
+
+class Player:
+    def take_shot(self):
+        while True:
+            try:
+                row = int(input("Enter the row (0-9): "))
+                col = int(input("Enter the column (0-9): "))
+                if 0 <= row <= 9 and 0 <= col <= 9:
+                    return row, col
+                else:
+                    print("Invalid input. Row and column must be between 0 and 9.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def check_shot(self, board, row, col):
+        # Code to check if the shot hits or misses goes here
         pass
 
 board = Board(10)
 board.place_ship(3)
 board.print_board()
+
+player = Player()
+shot_row, shot_col = player.take_shot()
