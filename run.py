3d24d7686fd693293cb3aa2e5a5841b69c0d7ca5
Erik Berger <berger.erik12@gmail.com>
2024-02-22 15:05:49 +0100

    Adding 5 different ships for class Player

--- run.py
+++ run.py
@@ -1,21 +1,16 @@
 from random import randint
 
 class Player:
-    def take_shot(self):
-        while True:
-            try:
-                row = int(input("Enter the row (0-9): "))
-                col = int(input("Enter the column (0-9): "))
-                if 0 <= row <= 9 and 0 <= col <= 9:
-                    return row, col
-                else:
-                    print("Invalid input. Row and column must be between 0 and 9.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
+    def __init__(self, name):
+        self.name = name
+        self.board = [["." for _ in range(10)] for _ in range(10)]
+        self.ships = {"carrier": 5, "battleship": 4, "cruiser": 3, "submarine": 3, "destroyer": 2}
+
+    def place_ships(self):
+        for ship, size in self.ships.items():
+            self.place_ship(ship, size)
 
     def check_shot(self, board, row, col):
-        # Code to check if the shot hits or misses goes here
         pass
 
-player = Player()
-shot_row, shot_col = player.take_shot()
+player = Player(10)
